3d059d73d0a93f70e21cc793c0d45a9b35b369eb
guizinsx <guizin51@gmail.com>
2024-03-27 11:01:24 -0300

    commit aula 2

--- app/src/main/java/com/example/intent/MainActivity.kt
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -1,10 +1,23 @@
 package com.example.intent
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_CHOOSER
+import android.content.Intent.ACTION_DIAL
+import android.content.Intent.ACTION_PICK
+import android.content.Intent.ACTION_VIEW
+import android.content.Intent.EXTRA_INTENT
+import android.content.Intent.EXTRA_TITLE
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
@@ -19,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var parl: ActivityResultLauncher<Intent>
+    private lateinit var pcarl: ActivityResultLauncher<String>
+    private lateinit var piarl: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
@@ -42,10 +58,31 @@ class MainActivity : AppCompatActivity() {
                         amb.parametroTv.text = it
                     }
                 }
+        }
+
+        pcarl = registerForActivityResult(ActivityResultContracts.RequestPermission()){ permissaoConcedida ->
+            if(permissaoConcedida){
+                chamarNumero(chamar = true)
+            }
+            else{
+                Toast.makeText(this, "Permissao necessaria para continuar", Toast.LENGTH_SHORT).show()
             }
 
+        }
+        piarl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ resultado ->
+            if (resultado.resultCode == RESULT_OK){
+                resultado.data?.data?.let { imagemUri ->
+                    amb.parametroTv.text = imagemUri.toString()
+                    Intent(ACTION_VIEW, imagemUri).also {
+                        startActivity(it)
+                    }
+                }
+                }
+            }
+        }
 
-    }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -54,13 +91,57 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
-            R.id.viewMi -> { true }
-            R.id.callMi -> { true }
-            R.id.dialMi -> { true }
-            R.id.pickMi -> { true }
-            R.id.chooserMi -> { true }
+            R.id.viewMi -> {
+                Uri.parse(amb.parametroTv.text.toString()).let{
+                    Intent(ACTION_VIEW, it).also {
+                        startActivity(it)
+                    }
+                }
+                true
+            }
+            R.id.callMi -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED){
+                        chamarNumero(chamar = true)
+                    }
+                }
+                else {
+                    pcarl.launch(CALL_PHONE)
+                }
+                true
+            }
+            R.id.dialMi -> {
+                chamarNumero(chamar = false)
+                true
+            }
+            R.id.pickMi -> {
+                val diretorioImagens =
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).path
+                val pegarImagemIntent =  Intent(ACTION_PICK)
+                pegarImagemIntent.setDataAndType(Uri.parse(diretorioImagens), "image/*")
+                piarl.launch(pegarImagemIntent)
+                true
+            }
+            R.id.chooserMi -> {
+                Uri.parse(amb.parametroTv.text.toString()).let{uri ->
+                    Intent(ACTION_VIEW, uri).also {navegadorIntent ->
+                        val escolherAppIntent = Intent(ACTION_CHOOSER)
+                        escolherAppIntent.putExtra(EXTRA_TITLE, "Escolha seu navegador favorito")
+                        escolherAppIntent.putExtra(EXTRA_INTENT, navegadorIntent)
+                        startActivity(escolherAppIntent)
+                    }
+                }
+                true
+            }
             else -> { false }
 
         }
+    }
+
+    private fun chamarNumero(chamar: Boolean){
+        val numeroUri: Uri = Uri.parse("tel:  ${amb.parametroTv.text}")
+        val chamarIntent: Intent = Intent(if (chamar)ACTION_CALL else ACTION_DIAL)
+        chamarIntent.data = numeroUri
+        startActivity(chamarIntent)
     }
 }
